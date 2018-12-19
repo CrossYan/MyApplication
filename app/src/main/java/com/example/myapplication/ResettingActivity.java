f93accfd6beb1459ccfa215535f65d210512ff63
CrossYan <851438603@qq.com>
2018-12-19 23:54:04 +0800

    add resetting ui

---
+++ app/src/main/java/com/example/myapplication/ResettingActivity.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class ResettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resetting);
+
+        final String content = "000000";
+        Button btn_reset = findViewById(R.id.button5);
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent();
+                data.putExtra("data",content);
+                setResult(2,data);
+                finish();
+
+            }
+        });
+
+
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
